174c3a548705427eed553e9f6a32564acd4d423c
TeoResende <resendenetto@yahoo.com.br>
2022-08-02 20:05:43 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,16 @@
+setTimeout(ligacao,500)
+
+function ligacao(){
+	
 var reset = document.getElementById("reset")
+var jx = document.getElementById("jx")
+var jo = document.getElementById("jo")
+
 
 
 reset.addEventListener("click",function(){
 	alert("Reiniciando o jogo!") 
 	document.location.reload(true)
 })
+}
 
