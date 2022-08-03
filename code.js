21bcac9d9c17474034d90d5584c4c641c13a3a30
TeoResende <resendenetto@yahoo.com.br>
2022-08-02 21:27:06 -0300

    Update code.js

--- code.js
+++ code.js
@@ -6,8 +6,6 @@ var reset = document.getElementById("reset")
 var jx = document.getElementById("jx")
 var jo = document.getElementById("jo")
 
-
-
 reset.addEventListener("click",function(){
 	alert("Reiniciando o jogo! "+jx.value+" VS "+jo.value) 
 	document.location.reload(true)
